ddf38d065cedb56d3548f05ec4794b2add6d0eb7
kimb0085 <kathleen.kimble@gmail.com>
2018-04-14 11:52:51 -0500

    cleaning up files

--- memory-game/src/components/ImageCard/ImageCard.js
+++ memory-game/src/components/ImageCard/ImageCard.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./ImageCard.css";
 
-
-// const ImageCard = props => (
-// 	<div className="card">
-// 		<div className="img-container">
-// 			<img alt={props.name} src={props.image} imageId={props.key} onClick={() => props.clickedImg(props.id)} />
-// 		</div>
-// 	</div>
-// );
-
 const ImageCard = (props) => {
 	return(
 	    <div className="card">
 	    	<div className="img-container">
-	        	<img alt={props.name} src={props.image} id={props.id} onClick={() => props.selectImage(props.id)}/> 
+	        	<img alt={props.name} src={props.image} id={props.id} onClick={() => props.selectedImage(props.id)}/> 
 	        </div> 
 	    </div>
 	   );
